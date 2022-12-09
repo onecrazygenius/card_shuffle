f8fcecb1fa280ff7341997fb717b4cb3003d6c49
Dev Popat <dev.popat@capgemini.com>
2022-12-09 11:38:07 +0000

    card v4

--- card.py
+++ card.py
@@ -21,15 +21,26 @@ class Card:
     def __str__(self):
         match self.id:
             case 1:
-                return str('Ace of ' + self.suit)
+                return str('Ace of ' + getSuit(self.suit))
             case 11:
-                return str('Jack of ' + self.suit)
+                return str('Jack of ' + getSuit(self.suit))
             case 12:
-                return str('Queen of ' + self.suit)
+                return str('Queen of ' + getSuit(self.suit))
             case 13:
-                return str('King of ' + self.suit)
+                return str('King of ' + getSuit(self.suit))
             case other:
-                return str( str(self.id) + ' of ' + self.suit)
+                return str( str(self.id) + ' of ' + getSuit(self.suit))
+
+    def getSuit(suit)
+        match suit:
+            case 's':
+                return 'Spades'
+            case 'd':
+                return 'Diamonds'
+            case 'h':
+                return 'Hearts'
+            case 'c':
+                return 'Clubs'
 
 
 
